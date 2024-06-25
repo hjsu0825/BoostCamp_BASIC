9e075199bec3284cf81ce04b88e6248d55ddd0a9
hjsu0825 <hjsu0825@naver.com>
2024-06-25 21:31:09 +0900

    [Feat] 시작지점 건너뛰기

--- Day2.js
+++ Day2.js
@@ -24,7 +24,7 @@ function play(moves){
         playerLoc[player] = (playerLoc[player] + move) % 16; // 현재 위치에 이동값을 더하고 16으로 나눈 값으로 보드판 순회
         let location = playerLoc[player]; // 이동 후 플레이어 위치
 
-        if (board[location] === null) { // 이동한 위치가 비어있으면
+        if (board[location] === null && location !== 0) { // 이동한 위치가 비어있고, 시작 지점이 아니면
             board[location] = player; // 플레이어가 해당 위치 소유
             playerCnt[player] += 1; // 해당 플레이어의 점유한 땅 개수 +1
         }
@@ -33,5 +33,31 @@ function play(moves){
     return playerCnt; // 모든 땅이 소유되면 종료 후 플레이어의 점유한 땅 개수 반환
 }
 
-const test1 = [1,2,3,4,1,1,1,1,2,2,2,2,3,3,3,3,1,1,1,1];
+// 테스트케이스
+const test1 = [
+    1,2,3,4,
+    1,1,1,2,
+    1,1,2,1,
+    1,1,1,2];
 console.log(play(test1));
+// { A: 1, B: 2, C: 1, D: 4 }
+const test2 = [
+    1, 1, 1, 3,
+    1, 1, 1, 1,
+    1, 1, 1, 4,
+    1, 1, 1, 1,
+    2, 2, 1, 3,
+    4, 4, 4, 2,
+    1, 1, 1, 2,
+    1, 1, 1, 1];
+console.log(play(test2));
+// { A: 5, B: 0, C: 1, D: 6 }
+const test3 = [
+    1, 2, 3, 4,
+    4, 4, 4, 4,
+    4, 4, 4, 4,
+    4, 4, 4, 4,
+    2, 3, 1, 3,
+    1, 3, 2, 4];
+console.log(play(test3));
+// { A: 4, B: 4, C: 4, D: 3 }
