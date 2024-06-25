61ed10f2b59fe92920e7200f69451a4720086e2e
hjsu0825 <hjsu0825@naver.com>
2024-06-25 21:19:31 +0900

    [Feat] 보드게임

---
+++ Day2.js
@@ -0,0 +1,37 @@
+/*
+A, B, C, D 4명의 참가자
+매 턴마다 1~4 사이 값 입력 제공
+모든 참가자는 시작 지점에서 출발
+한 번도 방문하지 않은 곳을 방문하면 방문자가 소유
+다른 사람이 이미 소유한 곳은 뺏을 수 없음
+모든 장소를 소유할 때까지 이동 가능
+입력값이 없거나 방문할 곳이 없으면 종료
+종료 시점에 소유한 장소 개수 리턴
+*/
+
+function play(moves){
+    const board = Array(16).fill(null); // 보드판
+    const players = ['A', 'B', 'C', 'D']; // 플레이어명
+    const playerLoc = {'A': 0, 'B': 0, 'C': 0, 'D': 0}; // 플레이어 현재 위치
+    const playerCnt = {'A': 0, 'B': 0, 'C': 0, 'D': 0}; // 플레이어 점유한 땅 개수
+
+    let idx = 0; // 이동값 인덱스 초기화
+
+    while (idx < moves.length) {
+        let player = players[idx % 4]; // 현재 턴의 플레이어
+        let move = moves[idx]; // 현재 턴의 이동값
+        
+        playerLoc[player] = (playerLoc[player] + move) % 16; // 현재 위치에 이동값을 더하고 16으로 나눈 값으로 보드판 순회
+        let location = playerLoc[player]; // 이동 후 플레이어 위치
+
+        if (board[location] === null) { // 이동한 위치가 비어있으면
+            board[location] = player; // 플레이어가 해당 위치 소유
+            playerCnt[player] += 1; // 해당 플레이어의 점유한 땅 개수 +1
+        }
+        idx += 1; // 이동값 인덱스 +1
+    }
+    return playerCnt; // 모든 땅이 소유되면 종료 후 플레이어의 점유한 땅 개수 반환
+}
+
+const test1 = [1,2,3,4,1,1,1,1,2,2,2,2,3,3,3,3,1,1,1,1];
+console.log(play(test1));
