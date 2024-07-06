847b15c242cbc1a18c7c4a9b05efe92c64fcc1cb
hjsu0825 <hjsu0825@naver.com>
2024-07-06 09:01:19 +0900

    리팩토링

--- Day10/main.js
+++ Day10/main.js
@@ -1,7 +1,7 @@
 const { LocationManager } = require("./location_manager.js");
 const fs = require("fs");
 
-const input = fs.readFileSync("./locations.txt").toString().trim().split("\n").map(location => location.split(" "));
+const input = fs.readFileSync("./Day10/locations.txt").toString().trim().split("\n").map(location => location.split(" "));
 
 const locationManager = new LocationManager(input);
 locationManager.kmeans_pop();
